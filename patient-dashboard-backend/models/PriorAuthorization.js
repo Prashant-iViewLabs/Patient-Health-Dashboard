929e5450e4628dfa4ed0fb588f920cf6830ac49f
jaydeep <jaydeep.parmar@itpreneurs.com>
2024-10-18 19:16:37 +0530

    timestamp-added-on-prior-authorization

--- patient-dashboard-backend/models/PriorAuthorization.js
+++ patient-dashboard-backend/models/PriorAuthorization.js
@@ -1,15 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const priorAuthorizationSchema = new mongoose.Schema({
-    patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
-    treatmentType: { type: String, required: true },
-    insurancePlan: { type: String, required: true },
-    doctorNotes: { type: String, required: false },
-    dateOfService: { type: Date, required: true },
-    diagnosisCode: { type: String, required: true },
-    status: { type: String, default: 'pending' } // Could be pending, approved, or denied
+  patientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+    required: true,
+  },
+  treatmentType: { type: String, required: true },
+  insurancePlan: { type: String, required: true },
+  doctorNotes: { type: String, required: false },
+  dateOfService: { type: Date, required: true },
+  diagnosisCode: { type: String, required: true },
+  status: { type: String, default: "pending" }, // Could be pending, approved, or denied
+  timestamp: { type: Date, default: Date.now },
 });
 
-const PriorAuthorization = mongoose.model('PriorAuthorization', priorAuthorizationSchema);
+const PriorAuthorization = mongoose.model(
+  "PriorAuthorization",
+  priorAuthorizationSchema
+);
 
 module.exports = PriorAuthorization;
